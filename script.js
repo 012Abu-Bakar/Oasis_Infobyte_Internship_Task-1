f2fe0117f204858f3bd45e701e0b137535f0b55e
Abu Bakar <89039978+012Abu-Bakar@users.noreply.github.com>
2023-03-03 09:05:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    function imgSlider(pop){
+    document.querySelector('.starbucks').src = pop;
+    }
+
+     function changeCircleColor(color){
+        const circle = document.querySelector('circle');
+        circle.style.background = circle; 
+     }  
